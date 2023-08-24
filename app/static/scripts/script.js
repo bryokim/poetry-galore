91189c4c2cf9bd5ec372e9e13cde42a049f212c0
Brian Kimathi <bryo.kim1@gmail.com>
2023-08-25 01:46:01 +0300

    Made the textarea dynamic

--- app/static/scripts/script.js
+++ app/static/scripts/script.js
@@ -4,4 +4,14 @@ $(document).ready(function () {
 
     this.style.height = this.scrollHeight + "px";
   });
+
+  $(".toggle-password").click(function () {
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    var input = $($(this).attr("toggle"));
+    if (input.attr("type") == "password") {
+      input.attr("type", "text");
+    } else {
+      input.attr("type", "password");
+    }
+  });
 });
