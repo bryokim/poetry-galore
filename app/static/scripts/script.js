a4015551b7b4a11b57251a49411178889fa911ec
Brian Kimathi <bryo.kim1@gmail.com>
2023-08-31 21:31:42 +0300

    Added search functionality
    User can now search using poem title or by clicking
    on a specific theme. Searching by theme only searches
    through the currently displayed poems.

--- app/static/scripts/script.js
+++ app/static/scripts/script.js
@@ -1,13 +1,12 @@
 $(document).ready(function () {
   $("TEXTAREA").on("input", function () {
     this.style.height = "auto";
-
     this.style.height = this.scrollHeight + "px";
   });
 
   $(".toggle-password").click(function () {
     $(this).toggleClass("fa-eye fa-eye-slash");
-    var input = $($(this).attr("toggle"));
+    let input = $($(this).attr("toggle"));
     if (input.attr("type") == "password") {
       input.attr("type", "text");
     } else {
@@ -23,29 +22,86 @@ $(document).ready(function () {
     $(`LI.comment-item P#${commentId}`).replaceWith(
       `<div id='${commentId}'>
       <input type="text" class="update-comment-input bg-body" id="${commentId}-1" value="${comment.trim()}">
-      <button type="button" class="update-comment-button btn-outline-success" onclick="updateComment('${poemId}', '${commentId}', '${commentId}-1')" name="update">Save</button>
+      <button type="button" class="update-comment-button btn-outline-success" name="update">Save</button>
       </div>`
     );
+
+    $("BUTTON.update-comment-button").on("click", function () {
+      newComment = $(`INPUT#${commentId}-1`).val();
+
+      $.get(
+        `http://127.0.0.1:5000/api/v1/poems/${poemId}/comments/${commentId}/update?text=${newComment.trim()}`,
+        function (data) {
+          console.log(data);
+        }
+      );
+
+      $(`LI.comment-item DIV#${commentId}`).replaceWith(
+        `<p
+          class="text-secondary font-monospace mb-0"
+          data-id="${commentId}"
+          id="${commentId}"
+          >
+          ${newComment.trim()}
+        </p>`
+      );
+    });
+
+    $(`INPUT#${commentId}-1`).on("keypress", function (event) {
+      if (event.which === 13) {
+        $("BUTTON.update-comment-button").trigger("click");
+      }
+    });
+  });
+
+  selectedThemes = [];
+
+  $(".theme-btn").on("click", function () {
+    $(this).toggleClass("active");
+    themeId = $(this).attr("id");
+
+    poemIds = [];
+    for (const element of $("DIV.poem-container")) {
+      poemIds.push(element.getAttribute("id"));
+    }
+
+    if ($(this).hasClass("active")) {
+      selectedThemes.push(themeId);
+
+      $.ajax({
+        type: "POST",
+        url: `http://127.0.0.1:5000/api/v1/search_by_theme`,
+        data: `{"themeIds": ${JSON.stringify(
+          selectedThemes
+        )}, "poemIds": ${JSON.stringify(poemIds)}}`,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        success: function (data) {
+          for (let id of data) {
+            $(`DIV#${id}`).hide();
+          }
+        },
+      });
+    } else {
+      selectedThemes = selectedThemes.filter((id) => id !== themeId);
+
+      $.ajax({
+        type: "POST",
+        url: `http://127.0.0.1:5000/api/v1/search_by_theme`,
+        data: `{"themeIds": ${JSON.stringify(
+          selectedThemes
+        )}, "poemIds": ${JSON.stringify(poemIds)}}`,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        success: function (data) {
+          validPoemIds = poemIds.filter((id) => !data.includes(id));
+          for (let id of validPoemIds) {
+            $(`DIV#${id}`).show();
+          }
+        },
+      });
+    }
   });
 });
-
-function updateComment(poemId, commentId, inputId) {
-  newComment = $(`INPUT#${inputId}`).val();
-
-  $.get(
-    `http://127.0.0.1:5000/api/v1/poems/${poemId}/comments/${commentId}/update?text=${newComment.trim()}`,
-    function (data) {
-      console.log(data);
-    }
-  );
-
-  $(`LI.comment-item DIV#${commentId}`).replaceWith(
-    `<p
-    class="text-secondary font-monospace mb-0"
-    data-id="${commentId}"
-    id="${commentId}"
-  >
-    ${newComment.trim()}
-  </p>`
-  );
-}
---
+++ app/views/core/search.py
@@ -0,0 +1,76 @@
+from flask import (
+    abort,
+    jsonify,
+    make_response,
+    render_template,
+    request,
+    url_for,
+)
+from flask_login import current_user, fresh_login_required, login_required
+
+from app.views import core_view
+from app.models.category import Category
+from app.models.poem import Poem
+from app.models.theme import Theme
+from app.models.engine.db_storage import DBStorage
+
+
+@core_view.route("/search")
+def search_poems():
+    """Search for poems matching the query parameter search.
+    The poem title is matched to the query parameter.
+
+    Returns:
+        response: Render home page with matching poems or
+            if search parameter is empty, return all poems.
+    """
+    if request.args.get("search"):
+        poems = DBStorage().all(Poem)
+        search_text = request.args.get("search")
+        valid_poems = {}
+
+        for key, poem in poems.items():
+            if search_text.lower() in poem.title.lower():
+                valid_poems[key] = poem
+
+        return render_template(
+            "accounts/home.html",
+            search=search_text,
+            poems=valid_poems.values(),
+            themes=DBStorage().all(Theme).values(),
+        )
+    else:
+        return render_template(
+            "accounts/home.html",
+            poems=DBStorage().all(Poem).values(),
+            themes=DBStorage().all(Theme).values(),
+        )
+
+
+@core_view.route("/search_by_theme", methods=["POST"])
+def search_by_theme():
+    data = request.get_json(silent=True)
+
+    if not data:
+        abort(400, description="Invalid JSON")
+
+    if data.get("themeIds") is None:
+        abort(400, description="Missing themeIds")
+
+    if not data.get("poemIds"):
+        abort(400, description="Missing poemIds")
+
+    themes = []
+    for themeId in data.get("themeIds"):
+        themes.append(DBStorage().get(Theme, themeId))
+
+    invalid_poem_ids = []
+    for poemId in data.get("poemIds"):
+        poem = DBStorage().get(Poem, poemId)
+
+        for theme in themes:
+            if theme not in poem.themes:
+                invalid_poem_ids.append(poemId)
+                break
+
+    return make_response(jsonify(invalid_poem_ids))
