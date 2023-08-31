91520385ee288f7e04cf7603e473ae03a2c8a1a6
Brian Kimathi <bryo.kim1@gmail.com>
2023-08-31 15:38:35 +0300

    Enabled CORS for the flask app

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ import os
 from decouple import config
 from flask import Flask
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -39,6 +40,12 @@ def create_app():
     login_manager.init_app(app)
     bcrypt.init_app(app)
 
+    CORS(
+        app,
+        resources={
+            r"*": {"origins": ["http://127.0.0.1", "http://localhost"]}
+        },
+    )
     # with app.before_request():
     #     app.config["storage"] = DBStorage(db)
 
