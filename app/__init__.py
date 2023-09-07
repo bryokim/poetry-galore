375982c3b542dbc29c90f601b668871ff875272b
Brian Kimathi <bryo.kim1@gmail.com>
2023-09-07 17:20:04 +0300

    Removed the accounts_view import and changed the default views for login_manager

--- app/__init__.py
+++ app/__init__.py
@@ -28,8 +28,8 @@ def create_app():
     except OSError:
         pass
 
-    login_manager.refresh_view = "accounts_view.login"
-    login_manager.login_view = "accounts_view.login"
+    login_manager.refresh_view = "core_view.login"
+    login_manager.login_view = "core_view.login"
     login_manager.login_message = "Login required."
     login_manager.needs_refresh_message = (
         "Please refresh your session before continuing"
@@ -53,11 +53,9 @@ def create_app():
     def load_user(user_id):
         return User.query.filter(User.alternative_id == user_id).first()
 
-    from app.views import accounts_view
     from app.views import api_view
     from app.views import core_view
 
-    app.register_blueprint(accounts_view)
     app.register_blueprint(api_view)
     app.register_blueprint(core_view)
     app.add_url_rule("/", endpoint="core_view.home")
