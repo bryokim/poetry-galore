b7c13f9172aa2dc3e9e331119271b1a41a1862f8
Brian Kimathi <bryo.kim1@gmail.com>
2023-09-08 19:28:50 +0300

    Added import statement for the search_api module

--- app/views/api/__init__.py
+++ app/views/api/__init__.py
@@ -2,5 +2,6 @@ from .categories_api import *
 from .comments_api import *
 from .likes_api import *
 from .poems_api import *
+from .search_api import *
 from .themes_api import *
 from .users_api import *
